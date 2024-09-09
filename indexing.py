3fb8037921aabb9d1c43977d73f89b5feda30fda
Ammarsaqib-programmer <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2024-09-09 17:56:58 +0500

    Update

---
+++ indexing.py
@@ -0,0 +1,14 @@
+import numpy as np
+array_A = np.array([[1,2,3],[4,5,6]])
+print(array_A.size)
+array_b=array_A.reshape(1,6)
+print(array_b)
+
+#Transpose
+array_b=array_A.T
+print(array_b)
+
+#to print specific column
+print(array_b[0])
+print(array_b[:,1])
+print(array_b[0,0])
