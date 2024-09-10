f2b6fc22008f485f22c2392d78549d75b1d0cdc2
Ammarsaqib-programmer <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2024-09-10 07:43:26 -0700

    Create array_with_random_number.py

---
+++ array_with_random_number.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Generate 20 random numbers between 0 and 1
+var = np.random.rand(20)
+print(var)
+
+# Generate 6 random integers between 1 and 200
+var_1 = np.random.randint(1, 200, 6)
+print(var_1)
