7fe32d15a11f8db2b962e6ada69f455d0fd80f7a
Ammarsaqib-programmer <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2024-09-10 21:50:16 -0700

    Create arthemetic_in_numpy.py

---
+++ arthemetic_in_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+var = np.array([1,2,3,4])
+varadd = var + 3
+print(varadd)
+
+var_1 = np.array([1,2,3,4])
+var_2 = np.array([2,3,4,5])
+varad = var_1 + var_2
+varad_1 = var_1 * var_2
+varad_2 = var_1 - var_2
+varad__3 = var_1 / var_2
+varad_4 = var_1 % var_2
+
+print(varad,"\n",varad_1,"\n",varad_2,"\n",varad__3,"\n",varad_4)
